1321713b28a404af04157d7f4f1ed59d99391a3e
Saad Shaikh <saadsk9288@gmail.com>
2023-01-13 01:21:23 +0530

    MAde form using Formik

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 const initialValues = {
   name: "",
@@ -14,82 +14,55 @@ const validationSchema = Yup.object({
   email: Yup.string().email("Invalid Email Format").required("Required"),
   channel: Yup.string().required("Required"),
 });
-const validate = (values) => {
-  let errors = {};
-  if (!values.name) {
-    errors.name = "Required";
-  }
-  if (!values.email) {
-    errors.email = "Required";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid email format";
-  }
-  if (!values.channel) {
-    errors.channel = "Required";
-  }
-  return errors;
-};
+// const validate = (values) => {
+//   let errors = {};
+//   if (!values.name) {
+//     errors.name = "Required";
+//   }
+//   if (!values.email) {
+//     errors.email = "Required";
+//   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+//     errors.email = "Invalid email format";
+//   }
+//   if (!values.channel) {
+//     errors.channel = "Required";
+//   }
+//   return errors;
+// };
 
 export const YoutubeForm = () => {
-  const formik = useFormik({
-    onSubmit,
-    initialValues,
-    // validate,
-    validationSchema,
-  });
-  // console.log("Form errors", formik.errors);
-  // console.log("Form values", formik.values);
-  //   console.log("Form touch", formik.touched);
   return (
     <>
-      <form onSubmit={formik.handleSubmit}>
-        <h1>Youtube Login Form</h1> <br />
-        <div className="form-control">
-          <label htmlFor="">Name:</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.name}
-          />
-          {formik.errors.name ? (
-            <div className="error">{formik.errors.name}</div>
-          ) : null}
-        </div>{" "}
-        <br />
-        <div className="form-control">
-          <label htmlFor="">E-mail</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.email}
-          />
-          {formik.errors.email ? (
-            <div className="error">{formik.errors.email}</div>
-          ) : null}
-        </div>
-        <br />
-        <div className="form-control">
-          <label htmlFor="">Channel</label>
-          <input
-            type="text"
-            name="channel"
-            id="channel"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.channel}
-          />
-          {formik.errors.channel ? (
-            <div className="error">{formik.errors.channel}</div>
-          ) : null}
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        <Form>
+          <h1>Youtube Login Form</h1> <br />
+          <div className="form-control">
+            <label htmlFor="">Name:</label>
+            <Field type="text" name="name" id="name" />
+           <ErrorMessage name="name"/>
+          </div>{" "}
+          <br />
+          <div className="form-control">
+            <label htmlFor="">E-mail</label>
+            <Field type="email" name="email" id="email" />
+            <ErrorMessage name="email"/>
+
+          </div>
+          <br />
+          <div className="form-control">
+            <label htmlFor="">Channel</label>
+            <Field type="text" name="channel" id="channel" />
+            <ErrorMessage name="channel"/>
+
+          </div>
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
     </>
   );
 };
+// Error Message tut 16 chalu hai 
