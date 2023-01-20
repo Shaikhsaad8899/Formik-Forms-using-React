12280ab6cd66df14d3ccece13e46c0bc03e91671
Saad Shaikh <saadsk9288@gmail.com>
2023-01-20 18:54:59 +0530

    Done dynamic index nested routing

--- src/Routing/Users/UserDetails.jsx
+++ src/Routing/Users/UserDetails.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
 
 export const UserDetails = () => {
+  const params = useParams();
+  const userId = params.userId;
   return (
-    <div>
-    <h1 htmlFor="">Name: Saad Shaikh</h1>
-    <h1 htmlFor="">D.O.B: 01/01/2001</h1>
-    <h1 htmlFor="">Place: Pune</h1>
-
+   <>
+     <div>
+      <h1 htmlFor="">Name: Saad Shaikh</h1>
+      <h1 htmlFor="">D.O.B: 01/01/2001</h1>
+      <h1 htmlFor="">Place: Pune</h1>
+      <h1 htmlFor="">User ID: {userId}</h1>
     </div>
-  )
-}
+   
+   </>
+    
+  );
+};
--- src/Routing/Users/Users.jsx
+++ src/Routing/Users/Users.jsx
@@ -1,15 +1,26 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useSearchParams } from "react-router-dom";
 
 export const Users = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
   return (
-    <div>
-      <ul>
-        <li>User 1 </li>
-        <li>User 2 </li>
-        <li>User 3 </li>
-        <Outlet />
-      </ul>
-    </div>
+    <>
+      <div>
+        <ul>
+          <li>User 1 </li>
+          <li>User 2 </li>
+          <li>User 3 </li>
+          <Outlet />
+        </ul>
+      </div>
+      <div>
+        <button onClick={() => setSearchParams({ filter: "active" })}>
+          Active Filter
+        </button>
+        <button onClick={() => setSearchParams({ filter: "reset" })}>
+          Reset Filter
+        </button>
+      </div>
+    </>
   );
 };
